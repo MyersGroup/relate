a2c49a1fc617d12147f83f14f4a81d509324e66f
Leo <leo.speidel@outook.com>
2018-06-12 21:20:43 +0100

    updated Mac binaries

---
+++ build/include/extract/CMakeFiles/RelateExtract.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RelateExtract.dir/RelateExtract.cpp.o"
+  "../../../bin/RelateExtract.pdb"
+  "../../../bin/RelateExtract"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RelateExtract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
