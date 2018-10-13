6a4ef0be12b3ce2accb80b579bc8567219e057a5
Leo <leo.speidel@outook.com>
2018-10-14 00:03:17 +0100

    Added mac binaries

---
+++ build/include/evaluate/CMakeFiles/RelateSelection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RelateSelection.dir/selection/RelateSelection.cpp.o"
+  "../../../bin/RelateSelection.pdb"
+  "../../../bin/RelateSelection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RelateSelection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
