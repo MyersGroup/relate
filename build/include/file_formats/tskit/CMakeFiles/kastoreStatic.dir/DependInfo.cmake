b2343d63af7a49fadfa05e8e607ec54c149655f6
Leo <leo.speidel@outook.com>
2019-05-03 16:12:31 +0100

    Compiled Mac binaries for v1.0.9

---
+++ build/include/file_formats/tskit/CMakeFiles/kastoreStatic.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/leo/Documents/genomics/relate/include/file_formats/tskit/kastore.c" "/Users/leo/Documents/genomics/relate/build/include/file_formats/tskit/CMakeFiles/kastoreStatic.dir/kastore.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  "../include/src"
+  "../include/test"
+  "../include/src/gzstream"
+  "../include/file_formats/tskit"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/include/file_formats/tskit/CMakeFiles/tskitStatic.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tskitStatic.dir/core.c.o"
+  "CMakeFiles/tskitStatic.dir/tables.c.o"
+  "CMakeFiles/tskitStatic.dir/trees.c.o"
+  "CMakeFiles/tskitStatic.dir/genotypes.c.o"
+  "CMakeFiles/tskitStatic.dir/convert.c.o"
+  "CMakeFiles/tskitStatic.dir/stats.c.o"
+  "../../../../bin/libtskitStatic.pdb"
+  "../../../../bin/libtskitStatic.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tskitStatic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
