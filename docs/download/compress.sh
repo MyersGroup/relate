b20c94470403e3c2331bf1451651aedac98dd51d
Leo <leo.speidel@outlook.com>
2019-05-14 16:49:27 +0100

    Need to compile Mac binaries (actually for v1.0.11)

---
+++ docs/download/compress.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+version="1.0.11"
+
+tar -czvf ./relate_v${version}_MacOSX.tgz relate_v${version}_MacOSX
+tar -czvf ./relate_v${version}_x86_64_dynamic.tgz relate_v${version}_x86_64_dynamic
+tar -czvf ./relate_v${version}_x86_64_static.tgz relate_v${version}_x86_64_static
+
