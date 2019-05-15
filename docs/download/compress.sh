d206c597ff82ba60457499f7fc7da893cc4ef353
Leo <leo.speidel@outook.com>
2019-05-16 00:56:43 +0100

    Updated to v1.0.12

--- docs/download/compress.sh
+++ docs/download/compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="1.0.11"
+version="1.0.12"
 
 tar -czvf ./relate_v${version}_MacOSX.tgz relate_v${version}_MacOSX
 tar -czvf ./relate_v${version}_x86_64_dynamic.tgz relate_v${version}_x86_64_dynamic
