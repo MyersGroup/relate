ed41abfa0271482918674651fb3544811ac01fb7
Leo <leo.speidel@outlook.com>
2020-05-25 17:45:00 +0100

    updated to v1.1.1

--- docs/download/compress.sh
+++ docs/download/compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="1.1.0"
+version="1.1.1"
 
 tar -czvf ./relate_v${version}_MacOSX.tgz relate_v${version}_MacOSX
 tar -czvf ./relate_v${version}_x86_64_dynamic.tgz relate_v${version}_x86_64_dynamic
