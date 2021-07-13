62a3f36033e5b4a571a96c84313ae47938793d57
Leo Speidel <leo.speidel@outlook.com>
2021-07-13 13:07:04 +0100

    Fixed bug in RelateMutationRate SummarizeForGenome

--- include/evaluate/mutation_rate/RelateMutationRate.cpp
+++ include/evaluate/mutation_rate/RelateMutationRate.cpp
@@ -490,7 +490,6 @@ void SummarizeWholeGenome(cxxopts::Options& options){
 
   //open files and add together
 
-  std::cerr << filenames[0] << std::endl;
   FILE* fp = fopen(filenames[0].c_str(),"rb");
   assert(fp != NULL);  
 
@@ -498,7 +497,7 @@ void SummarizeWholeGenome(cxxopts::Options& options){
   std::vector<double> epochs;
   fread(&num_epochs, sizeof(int), 1, fp);
   epochs.resize(num_epochs);
-  fread(&epochs[0], sizeof(float), num_epochs, fp);
+  fread(&epochs[0], sizeof(double), num_epochs, fp);
 
   CollapsedMatrix<double> mut_by_type_and_epoch, mut_by_type_and_epoch_tmp;
   mut_by_type_and_epoch.ReadFromFile(fp);
@@ -508,7 +507,7 @@ void SummarizeWholeGenome(cxxopts::Options& options){
     fp = fopen(filenames[i].c_str(),"rb");
 
     fread(&num_epochs, sizeof(int), 1, fp);
-    fread(&epochs[0], sizeof(float), num_epochs, fp);
+    fread(&epochs[0], sizeof(double), num_epochs, fp);
 
     mut_by_type_and_epoch_tmp.ReadFromFile(fp);
     fclose(fp);
@@ -552,7 +551,7 @@ void SummarizeWholeGenome(cxxopts::Options& options){
 
   fp = fopen((options["output"].as<std::string>() + "_mut" + ".bin" ).c_str(), "wb"); 
   fwrite(&num_epochs, sizeof(int), 1, fp);
-  fwrite(&epochs[0], sizeof(float), epochs.size(), fp);
+  fwrite(&epochs[0], sizeof(double), epochs.size(), fp);
   mut_by_type_and_epoch.DumpToFile(fp);
   fclose(fp);
   fp = fopen((options["output"].as<std::string>() + "_opp" + ".bin" ).c_str(), "wb");  
