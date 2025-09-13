9522ee35e10b114b6c17324a887335b4679c2c37
Leo Speidel <leo.speidel@outlook.com>
2025-09-13 22:56:13 +0900

    Updated to v1.2.4

--- docs/download/compress.sh
+++ docs/download/compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="1.2.3"
+version="1.2.4"
 
 tar -czvf ./relate_v${version}_MacOSX_Intel.tgz relate_v${version}_MacOSX_Intel
 tar -czvf ./relate_v${version}_MacOSX_M.tgz relate_v${version}_MacOSX_M
